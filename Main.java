b8e57adf3e39ecc658af3713445687ce41c895f0
carahkatz <carah.katz@gmail.com>
2024-02-14 17:48:21 -0500

    Sphere class coded. Main class coded. Test cases updated. Design updated.

--- Main.java
+++ Main.java
@@ -13,8 +13,20 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-       System.out.println("Music of the Spheres");
+        System.out.println("Music of the Spheres\n");
 
+        Sphere sun = new Sphere(0.0, 0.0, 0.0, 43.26);
+        Sphere earth = new Sphere(26.0, 42.0, 79.0, 0.395);
+        Sphere planetX = new Sphere(25.3, 43.1, 77.8, 1.4);
+
+        System.out.println("Now calculating for... Sun.");
+        System.out.println("Sun Area: " + sun.area() + "\nSun Volume: " + sun.volume() + "\nSun Distance: " + sun.distance());
+
+        System.out.println("\nNow calculating for... Earth.");
+        System.out.println("Earth Area: " + earth.area() + "\nEarth Volume: " + earth.volume() + "\nEarth Distance: " + earth.distance());
+
+        System.out.println("\nNow calculating for... PlanetX.");
+        System.out.println("PlanetX Area: " + planetX.area() + "\nPlanetX Volume: " + planetX.volume() + "\nPlanetX Distance: " + planetX.distance());
 
     }
 }
--- Sphere.java
+++ Sphere.java
@@ -1,20 +1,65 @@
 
-/** Programmers:  [your names here]
+/** Programmers:  Aidan and Carah
 * Course:  CS 212
 * Due Date:
-* Lab Assignment:
-* Problem Statement:
+* Lab Assignment: The music of spheres
+* Problem Statement: Using classes, create spheres for the sun, earth, and planetX
 * Data In:
 * Data Out:
 * Credits: [Is your code based on an example in the book, in class, or something else?
 *            Reminder: you should never take code from the Internet or another person
 */
 
-
+import java.lang.Math;
+import java.util.Scanner;
 class Sphere {
-    public static void main(String[] args) {
-       System.out.println("Spheres Go Here");
+    Scanner input = new Scanner(System.in);
+    // Initialize necessary variables
+    double xCoord1;
+    double yCoord1;
+    double zCoord1;
+    double planetRadius;
+    public double area;
+    public double volume;
+    public double distance;
 
+    // Default constructor
+    // Take in values from Main call instance
+    public Sphere(double x, double y, double z, double radius) {
+        xCoord1 = x;
+        yCoord1 = y;
+        zCoord1 = z;
+        planetRadius = radius;
+    }
 
+    // Area method
+    public double area() {
+        // area = 4 * pi * r^2
+        area = 4 * Math.PI * Math.pow(planetRadius,2);
+        return area;
+    }
+
+    // Volume method
+    public double volume() {
+        // volume = (4/3) * pi * r^3
+        volume = (double) (4/3) * Math.PI * Math.pow(planetRadius, 3);
+        return volume;
+    }
+
+    // Distance method
+    public double distance() {
+        // Get second coords from user
+        System.out.println("What is the second x point? ");
+        double xCoord2 = input.nextDouble();
+
+        System.out.println("What is the second y point? ");
+        double yCoord2 = input.nextDouble();
+
+        System.out.println("What is the second z point? ");
+        double zCoord2 = input.nextDouble();
+
+        // distance = sqrt((x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2)
+        distance = Math.sqrt(Math.pow((xCoord1-xCoord2), 2) + Math.pow((yCoord1-yCoord2), 2) + Math.pow(zCoord1-zCoord2, 2));
+        return distance;
     }
 }
